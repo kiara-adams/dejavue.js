508f554a8771468c9962e3995de7f3c1a7bd8aa7
kiara <kiara.admams.032006@gmail>
2024-06-24 14:57:26 +0200

    add

---
+++ src/components/CardComp.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="card" :style="{ borderColor: gender === 'ale'? 'gold' : 'green' }">
+      <slot name="header"></slot>
+      <slot name="content"></slot>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      gender: {
+        type: String,
+        required: true
+      }
+    }
+  }
+  </script>
+  
+  <style>
+  .card {
+    border: 1px solid;
+    padding: 10px;
+    margin: 10px;
+  }
+  </style>
